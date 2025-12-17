2a11ce98f6b12f667394e94b1ade1e77928a32ec
WT-SL <sahil.lodha@walkweltech.com>
2025-12-17 17:41:43 +0530

    fixes build issue

--- src/components/common/ProductVariationsSearch/ProductVariationsSearch.tsx
+++ src/components/common/ProductVariationsSearch/ProductVariationsSearch.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Info, Plus, X } from "lucide-react";
+import { Info, Plus } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
