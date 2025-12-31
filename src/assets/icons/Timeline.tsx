e44db81df1c1cfab173ca3bc22ba5784d7e93e50
ayush.chauhan <ayush.chauhan@walkweltech.com>
2025-12-31 11:39:00 +0530

    warning bug fixes for svg icons

--- src/assets/icons/Timeline.tsx
+++ src/assets/icons/Timeline.tsx
@@ -16,20 +16,20 @@ export default function TimelineSvg({
       <path
         d="M11.8684 0.5H1.44737C0.924151 0.5 0.5 0.924151 0.5 1.44737V8.55263C0.5 9.07585 0.924151 9.5 1.44737 9.5H11.8684C12.3916 9.5 12.8158 9.07585 12.8158 8.55263V1.44737C12.8158 0.924151 12.3916 0.5 11.8684 0.5Z"
         stroke={color}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M0.5 3.81641H12.8158"
         stroke={color}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.02637 7.13086H10.4474"
         stroke={color}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
